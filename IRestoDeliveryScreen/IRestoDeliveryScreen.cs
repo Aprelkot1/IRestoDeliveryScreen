97ab6366be9e842aa0ddeae69a6587d76e3de5cf
ivanushka2014 <ivanushka.lu@gmail.com>
2022-11-30 16:19:26 +0300

    Добавьте файлы проекта.

---
+++ IRestoDeliveryScreen/IRestoDeliveryScreen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting;
+using Resto.Front.Api;
+using Resto.Front.Api.Attributes;
+using Resto.Front.Api.Attributes.JetBrains;
+
+namespace Resto.Front.Api.IRestoDeliveryScreen
+{
+    [UsedImplicitly]
+    [PluginLicenseModuleId(21016318)]
+    public class IRestoDeliveryScreen : IFrontPlugin
+    {
+        private readonly Stack<IDisposable> subscriptions = new Stack<IDisposable>();
+
+        public IRestoDeliveryScreen()
+        {
+            PluginContext.Log.Info("IRestoDeliveryScreen launched");
+            subscriptions.Push(new Window1());
+        }
+
+        public void Dispose()
+        {
+            while (subscriptions.Any())
+            {
+                var subscription = subscriptions.Pop();
+                try
+                {
+                    subscription.Dispose();
+                }
+                catch (RemotingException)
+                {
+                    // nothing to do with the lost connection
+                }
+            }
+
+            PluginContext.Log.Info("IRestoTroubleshooter stopped");
+        }
+    }
+}
---
+++ IRestoDeliveryScreen/Window1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Resto.Front.Api;
+namespace Resto.Front.Api.IRestoDeliveryScreen
+{
+    using static PluginContext;
+    public class Window1 : IDisposable
+    {
+        public Window1()
+        {
+            Log.Info("Window1 created");
+        }
+        public void Dispose()
+        {
+            Log.Info("Window1 disposed");
+        }
+    }
+}
